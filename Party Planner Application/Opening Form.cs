3545a79560207bf6a61306042efdf8df64739776
Jordan Mendez <jem2000men@gmail.com>
2020-11-24 16:03:02 -0600

    Database altered

--- Party Planner Application/Opening Form.cs
+++ Party Planner Application/Opening Form.cs
@@ -19,12 +19,22 @@ namespace Party_Planner_Application
 
         private void openingForm_Load(object sender, EventArgs e)
         {
-           
+            // TODO: This line of code loads data into the 'partyDatabaseDataSet.Guests' table. You can move, or remove it, as needed.
+            this.guestsTableAdapter.Fill(this.partyDatabaseDataSet.Guests);
+
         }
 
         private void calenderButton_Click(object sender, EventArgs e)
         {
           
+        }
+
+        private void guestsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.guestsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.partyDatabaseDataSet);
+
         }
     }
 }
