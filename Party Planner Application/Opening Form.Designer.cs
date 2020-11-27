bf2b1af6490df1c16eec307adfcf52d81de6b9bd
JohnIsMyUserName <75029209+JohnIsMyUserName@users.noreply.github.com>
2020-11-26 22:41:21 -0600

    Update Opening Form.Designer.cs

--- Party Planner Application/Opening Form.Designer.cs
+++ Party Planner Application/Opening Form.Designer.cs
@@ -28,90 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.partyBox = new System.Windows.Forms.ListBox();
+            this.mainMenuPanel = new System.Windows.Forms.Panel();
+            this.parttyListButton = new System.Windows.Forms.Button();
             this.addPartyButton = new System.Windows.Forms.Button();
-            this.calenderButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
+            this.calendarButton = new System.Windows.Forms.Button();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.logoPanel2 = new System.Windows.Forms.Panel();
+            this.mainMenuPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // mainMenuPanel
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
+            this.mainMenuPanel.AutoScroll = true;
+            this.mainMenuPanel.BackColor = System.Drawing.Color.Black;
+            this.mainMenuPanel.Controls.Add(this.parttyListButton);
+            this.mainMenuPanel.Controls.Add(this.addPartyButton);
+            this.mainMenuPanel.Controls.Add(this.calendarButton);
+            this.mainMenuPanel.Controls.Add(this.logoPanel);
+            this.mainMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.mainMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuPanel.Name = "mainMenuPanel";
+            this.mainMenuPanel.Size = new System.Drawing.Size(221, 561);
+            this.mainMenuPanel.TabIndex = 1;
             // 
-            // splitContainer1.Panel1
+            // parttyListButton
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.partyBox);
-            this.splitContainer1.Panel1.Controls.Add(this.addPartyButton);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.calenderButton);
-            this.splitContainer1.Size = new System.Drawing.Size(1010, 634);
-            this.splitContainer1.SplitterDistance = 336;
-            this.splitContainer1.TabIndex = 0;
-            // 
-            // partyBox
-            // 
-            this.partyBox.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.partyBox.FormattingEnabled = true;
-            this.partyBox.HorizontalScrollbar = true;
-            this.partyBox.ItemHeight = 46;
-            this.partyBox.Location = new System.Drawing.Point(0, 108);
-            this.partyBox.Name = "partyBox";
-            this.partyBox.Size = new System.Drawing.Size(336, 602);
-            this.partyBox.TabIndex = 1;
+            this.parttyListButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.parttyListButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.parttyListButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.parttyListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parttyListButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.parttyListButton.Location = new System.Drawing.Point(0, 202);
+            this.parttyListButton.Name = "parttyListButton";
+            this.parttyListButton.Size = new System.Drawing.Size(221, 51);
+            this.parttyListButton.TabIndex = 3;
+            this.parttyListButton.Text = "Party List";
+            this.parttyListButton.UseVisualStyleBackColor = true;
             // 
             // addPartyButton
             // 
-            this.addPartyButton.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addPartyButton.Location = new System.Drawing.Point(0, 9);
+            this.addPartyButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.addPartyButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.addPartyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addPartyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addPartyButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.addPartyButton.Location = new System.Drawing.Point(0, 151);
             this.addPartyButton.Name = "addPartyButton";
-            this.addPartyButton.Size = new System.Drawing.Size(336, 93);
-            this.addPartyButton.TabIndex = 0;
+            this.addPartyButton.Size = new System.Drawing.Size(221, 51);
+            this.addPartyButton.TabIndex = 2;
             this.addPartyButton.Text = "Add Party";
             this.addPartyButton.UseVisualStyleBackColor = true;
             // 
-            // calenderButton
+            // calendarButton
             // 
-            this.calenderButton.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.calenderButton.Location = new System.Drawing.Point(252, 363);
-            this.calenderButton.Name = "calenderButton";
-            this.calenderButton.Size = new System.Drawing.Size(162, 72);
-            this.calenderButton.TabIndex = 0;
-            this.calenderButton.Text = "Check Calender";
-            this.calenderButton.UseVisualStyleBackColor = true;
-            this.calenderButton.Click += new System.EventHandler(this.calenderButton_Click);
+            this.calendarButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.calendarButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.calendarButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calendarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calendarButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.calendarButton.Location = new System.Drawing.Point(0, 100);
+            this.calendarButton.Name = "calendarButton";
+            this.calendarButton.Size = new System.Drawing.Size(221, 51);
+            this.calendarButton.TabIndex = 1;
+            this.calendarButton.Text = "Calendar";
+            this.calendarButton.UseVisualStyleBackColor = true;
+            // 
+            // logoPanel
+            // 
+            this.logoPanel.BackgroundImage = global::Party_Planner_Application.Properties.Resources.LogoHat;
+            this.logoPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.logoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoPanel.Location = new System.Drawing.Point(0, 0);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(221, 100);
+            this.logoPanel.TabIndex = 0;
+            // 
+            // logoPanel2
+            // 
+            this.logoPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.logoPanel2.BackgroundImage = global::Party_Planner_Application.Properties.Resources.LogoHat2;
+            this.logoPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.logoPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoPanel2.Location = new System.Drawing.Point(221, 0);
+            this.logoPanel2.Name = "logoPanel2";
+            this.logoPanel2.Size = new System.Drawing.Size(713, 561);
+            this.logoPanel2.TabIndex = 2;
             // 
             // openingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1010, 634);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.logoPanel2);
+            this.Controls.Add(this.mainMenuPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
             this.Name = "openingForm";
             this.Text = "Party Planner";
             this.Load += new System.EventHandler(this.openingForm_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.mainMenuPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel mainMenuPanel;
+        private System.Windows.Forms.Button parttyListButton;
         private System.Windows.Forms.Button addPartyButton;
-        private System.Windows.Forms.ListBox partyBox;
-        private System.Windows.Forms.Button calenderButton;
+        private System.Windows.Forms.Button calendarButton;
+        private System.Windows.Forms.Panel logoPanel;
+        private System.Windows.Forms.Panel logoPanel2;
     }
 }
-
