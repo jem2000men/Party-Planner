fadc89c3167d6aaefb4f46d81c5933f0ecf24210
cjkriska <56700349+cjkriska@users.noreply.github.com>
2020-11-27 17:43:34 -0600

    Party Creation and Database Connection

---
+++ Party Planner Application/Party-Planner-main/Party Planner Application/dbo.Parties.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Parties] (
+    [PartyId]    INT           IDENTITY (1, 1) NOT NULL,
+    [Party Name] NVARCHAR (50) NOT NULL,
+    [Date]       DATETIME      NULL,
+    [Location]   NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([PartyId] ASC)
+);
+
