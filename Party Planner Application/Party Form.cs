7ab1d6fddb7acce16106180d62c47fb0f24fb9e6
Jordan Mendez <jem2000men@gmail.com>
2020-11-30 18:01:10 -0600

    Added comments to party form

--- Party Planner Application/Party Form.cs
+++ Party Planner Application/Party Form.cs
@@ -15,14 +15,14 @@ namespace Party_Planner_Application
 {
     public partial class Party_Form : Form
     {
-        int currentparty;
-        public Party_Form(int currentparty)
+        int currentparty; //int to knwo what party is being managed
+        public Party_Form(int currentparty) //constructor to initialize int variable
         {
             InitializeComponent();
             this.currentparty = currentparty;
         }
 
-        private void partiesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        private void partiesBindingNavigatorSaveItem_Click(object sender, EventArgs e) //to save changes
         {
             this.Validate();
             this.partiesBindingSource.EndEdit();
@@ -32,6 +32,7 @@ namespace Party_Planner_Application
 
         private void Party_Form_Load(object sender, EventArgs e)
         {
+            //Load data into all tables
             // TODO: This line of code loads data into the 'partyDatabaseDataSet.Items' table. You can move, or remove it, as needed.
             this.itemsTableAdapter.FillByPartyId(this.partyDatabaseDataSet.Items, currentparty);
             // TODO: This line of code loads data into the 'partyDatabaseDataSet.Guests' table. You can move, or remove it, as needed.
@@ -42,7 +43,7 @@ namespace Party_Planner_Application
 
         }
 
-        private void saveButton_Click(object sender, EventArgs e)
+        private void saveButton_Click(object sender, EventArgs e) //to save changes
         {
             try
             {
@@ -56,12 +57,12 @@ namespace Party_Planner_Application
             }
         }
 
-        private void cancelButton_Click(object sender, EventArgs e)
+        private void cancelButton_Click(object sender, EventArgs e) //to undo changes just refill table
         {
             this.partiesTableAdapter.FillByPartyId(this.partyDatabaseDataSet.Parties, currentparty);
         }
 
-        private void addGuestButton_Click(object sender, EventArgs e)
+        private void addGuestButton_Click(object sender, EventArgs e) //to add guest open form and save changes
         {
             NewGuestForm newForm = new NewGuestForm(currentparty);
             newForm.ShowDialog();
@@ -72,7 +73,7 @@ namespace Party_Planner_Application
         }
 
         private void manageGuestButton_Click(object sender, EventArgs e)
-        {
+        {//Open manage guest form and save changes
             ManageGuestForm mgForm = new ManageGuestForm(currentparty, int.Parse(guestListBox.SelectedValue.ToString()));
             mgForm.ShowDialog();
             this.guestsTableAdapter.FillByPartyId(this.partyDatabaseDataSet.Guests, currentparty);
@@ -83,12 +84,14 @@ namespace Party_Planner_Application
 
         private void addSupplyButton_Click(object sender, EventArgs e)
         {
+            //makes it visible to add items
             ItemTextBox.Visible = true;
             AddItemButton.Visible = true;
         }
 
         private void AddItemButton_Click(object sender, EventArgs e)
         {
+            //if item text box not empty, add item and update tables
             if (ItemTextBox.Text != "")
             {
                 this.itemsTableAdapter.InsertItem(-1, ItemTextBox.Text, currentparty);
@@ -102,16 +105,17 @@ namespace Party_Planner_Application
             }
         }
 
-        private void partyReportButton_Click(object sender, EventArgs e)
-        {
-            var reportDoc = DocX.Create("report.docx");
+        private void partyReportButton_Click(object sender, EventArgs e) //my personal favorite
+        { //Uses DocX nuget package
+            var reportDoc = DocX.Create("report.docx"); //create document
+            //create strings to put in word document
             string titleText = "Party Report\n";
             string reportInfo = "Party Name: " + party_NameTextBox.Text + "\n";
             reportInfo += "Date: " + dateDateTimePicker.Value.ToString("d") + "\n";
             reportInfo += "Location: " + locationTextBox.Text + "\n\n";
             string guestInfo = "Invited Guests:\n";
             string itemInfo = "Items:\n";
-
+            //add all guests to string
             if (guestListBox.Items.Count == 0)
             {
                 guestInfo += "(none)\n\n";
@@ -125,11 +129,11 @@ namespace Party_Planner_Application
                     {
                         guestListBox.SelectedIndex += 1;
                         guestInfo += first_NameLabel1.Text + " " + last_NameLabel1.Text + " bringing ";
-                        if (itemLabel1.Text == "")
+                        if (itemLabel1.Text == "") //if guest bringing nothing say nothing
                         {
                             guestInfo += "nothing.\n";
                         }
-                        else
+                        else //else add item brought by guest
                         {
                             guestInfo += itemLabel1.Text + ".\n";
                         }
@@ -142,6 +146,7 @@ namespace Party_Planner_Application
                 guestListBox.SelectedIndex = -1;
                 guestInfo += "\n";
             }
+            //do same for items
             if (ItemBox.Items.Count == 0)
             {
                 itemInfo += "(none)\n";
@@ -163,11 +168,13 @@ namespace Party_Planner_Application
                 }
                 ItemBox.SelectedIndex = -1;
             }
-
+            //create formatting variables
             var titleFormat = new Formatting();
             var infoFormat = new Formatting();
+            //make formats bigger, title format biggest
             infoFormat.Size = 16;
             titleFormat.Size = 26;
+            //insert paragraphs with correct format, with no track changes
             reportDoc.InsertParagraph(titleText, false, titleFormat);
             reportDoc.InsertParagraph(reportInfo, false, infoFormat);
             reportDoc.InsertParagraph(guestInfo, false, infoFormat);
@@ -178,11 +185,11 @@ namespace Party_Planner_Application
             
             
             reportDoc.Save();
-            Process.Start("WINWORD.EXE", "report.docx");
+            Process.Start("WINWORD.EXE", "report.docx"); //save and open document
         }
 
         private void removeItem_Click(object sender, EventArgs e)
-        {
+        { //delete item and save and update table
             this.itemsTableAdapter.DeleteItem(currentparty, ItemBox.SelectedValue.ToString());
             this.itemsTableAdapter.FillByPartyId(this.partyDatabaseDataSet.Items, currentparty);
             this.Validate();
